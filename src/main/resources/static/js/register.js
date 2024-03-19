52a541e1a3dfce2aaa365e7751ee17987d679aa2
Dengeatz <gameone.exe@gmail.com>
2024-03-19 17:22:41 +0500

    registration page update

---
+++ src/main/resources/static/js/register.js
@@ -0,0 +1,9 @@
+function anim() {
+
+    document.querySelector('#termsOfAgree').style.top = '200px';
+}
+
+function hideRegister() {
+
+    document.querySelector('#termsOfAgree').style.top = '-600px';
+}
